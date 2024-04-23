76404214f2f600d745eeaa02160359649889ffbb
ofek-zilber-icbc <Ofek.Zilbershtein@icbc.com>
2024-04-23 13:12:15 -0700

    added dummy data for http function

--- backend/integration/function_app.py
+++ backend/integration/function_app.py
@@ -1,5 +1,6 @@
 import azure.functions as func
 import logging
+import json
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -16,10 +17,38 @@ def HttpIntegration(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    response = {
+        "representative": "Adrian Smith",
+        "transcription": '"Speaker0": Hi there. I\'m caller from ICBC. My name is Eddie. I\'m just here looking for Bobby, please. "Speaker1": Hi, Eddie, this is Bobby speaking.',
+        "customerName": "Bobby",
+        "summary": "Call the customer to check status of treatment plan",
+        "callLength": 15,
+        "flags": [
+            {
+                "isCustomer": True,
+                "isRepresentative": False,
+                "timestamp": 2
+            },
+            {
+                "isCustomer": True,
+                "isRepresentative": True,
+                "timestamp": 4
+            },
+            {
+                "isCustomer": False,
+                "isRepresentative": True,
+                "timestamp": 10
+            }
+        ]
+    }
+
+    return func.HttpResponse(json.dumps(response),
+                status_code=200                
+            )
+    # if name:
+    #     return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    # else:
+    #     return func.HttpResponse(
+    #          "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+    #          status_code=200
+    #     )
