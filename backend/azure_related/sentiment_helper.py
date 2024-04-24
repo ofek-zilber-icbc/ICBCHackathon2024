bb5a9b5c2bdda4824eb9819d09b3897ffa839521
ofek-zilber-icbc <Ofek.Zilbershtein@icbc.com>
2024-04-24 16:23:20 -0700

    transcription & flagging

--- backend/azure_related/sentiment_helper.py
+++ backend/azure_related/sentiment_helper.py
@@ -3,8 +3,8 @@ from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 
 # Set up the Azure Text Analytics client
-text_analytics_key = os.environ.get('TEXT_KEY')
-text_analytics_endpoint = os.environ.get('TEXT_ENDPOINT')
+text_analytics_key = os.environ.get('LANGUAGE_KEY')
+text_analytics_endpoint = os.environ.get('LANGUAGE_ENDPOINT')
 
 credential = AzureKeyCredential(text_analytics_key)
 text_analytics_client = TextAnalyticsClient(endpoint=text_analytics_endpoint, credential=credential)
@@ -12,6 +12,8 @@ text_analytics_client = TextAnalyticsClient(endpoint=text_analytics_endpoint, cr
 def perform_sentiment_analysis(text):
     documents = [text]
     response = text_analytics_client.analyze_sentiment(documents=documents)[0]
+    print("response: {}".format(response))
+    flagged = flag_sentences(response)
     
     sentiment = response.sentiment
     confidence_scores = {
@@ -20,4 +22,16 @@ def perform_sentiment_analysis(text):
         "negative": response.confidence_scores.negative
     }
     
-    return sentiment, confidence_scores
+    print("flagged: {}".format(flagged))
+
+    return flagged
+
+
+def flag_sentences(response):
+    flags = []
+    for sentence in response.sentences:
+        if sentence.sentiment == 'negative' and sentence.confidence_scores.negative >= 0.85:
+            flags.append(sentence)
+    return flags
+
+perform_sentiment_analysis("Like I, you know, it's so disruptive to my life.")
